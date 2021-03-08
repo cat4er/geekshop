7f264c897fac11b886d6d66638f7efcdc8fed398
cat4er <mooncat4er@gmail.com>
2021-03-08 22:40:49 +0300

    lesson_10. Social auth

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,23 +1,7 @@
-"""geekshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
-from django.urls import re_path
+from django.urls import path, re_path
 
 import mainapp.views as mainapp
 
@@ -25,6 +9,7 @@ urlpatterns = [
     re_path(r"^$", mainapp.main, name="main"),
     re_path(r"^products/", include("mainapp.urls", namespace="products")),
     re_path(r"^contact/", mainapp.contact, name="contact"),
+    path("", include("social_django.urls", namespace="social")),
     re_path(r"^auth/", include("authnapp.urls", namespace="auth")),
     re_path(r"^basket/", include("basketapp.urls", namespace="basket")),
     re_path(r"^admin/", include("adminapp.urls", namespace="admin")),
