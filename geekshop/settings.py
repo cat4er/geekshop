a32b0a1d5816e432e6eb4abae5f7c0123716ff65
cat4er <mooncat4er@gmail.com>
2021-03-29 01:13:49 +0300

    lesson_15. Cache entire site + screenshots

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -56,14 +56,16 @@ if DEBUG:
 AUTH_USER_MODEL = "authnapp.ShopUser"
 
 MIDDLEWARE = [
+    "django.middleware.cache.UpdateCacheMiddleware",  # for entire site caching
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "social_django.middleware.SocialAuthExceptionMiddleware",
+    "django.middleware.cache.FetchFromCacheMiddleware",  # for entire site caching
 ]
 
 if DEBUG:
